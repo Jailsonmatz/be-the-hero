ae6a553080f27e39f34c1f7e1ed1df3d02eab3ca
Jailson Home <jailsonmatz@outlook.com>
2020-03-29 14:43:29 -0300

    Testes da aplicacao

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express'); // express é um mini framework de rotas MVC
+const {errors} = require('celebrate');
 const routes = require('./routes');
 const cors = require('cors');
 
@@ -7,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // diz ao express que as requisições serão em JSON
 app.use(routes); // usa as rotas importadas do arquivo routes.js
+app.use(errors());
 
-app.listen(3333);
+module.exports = app;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const {celebrate, Segments, Joi} = require('celebrate');
+
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -6,16 +8,65 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+/*
+ * QUERY = Vem pela URL com variavel setada ex: /?name=matz
+ * BODY = corpo da requisicao 
+ * PARAMS = É o que vem da url sem variavel setada ex: /:id    
+ * HEADER = É a parte de configuracao ou autorizacao
+*/
+
+routes.post('/sessions',celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required()
+    })   
+}), SessionController.create);
 
 routes.get('/ongs', OngController.index );
-routes.post('/ongs', OngController.create );
 
-routes.get('/profile', ProfileController.index);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })    
+}), OngController.create );
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()   
+    }).unknown()
+}), ProfileController.index);
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.index);
+
+
+routes.post('/incidents', celebrate({
+
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required().max(20),
+        description: Joi.string().required().max(100),
+        value: Joi.number().required()
+    }),
+
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()   
+    }).unknown()
+
+}), IncidentController.create);
+
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete);
 
 // serve para exportar uma variavel e usar em outro lugar
 module.exports = routes;
